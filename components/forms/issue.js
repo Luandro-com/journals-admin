7347e1407dc6075b4731193f07de15f902084a23
luandro <luandro@gmail.com>
2018-12-13 23:21:52 -0200

    start implementing editor

--- components/forms/issue.js
+++ components/forms/issue.js
@@ -10,6 +10,7 @@ import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
 import Upload from './Upload'
 import OutlineTextField from './OutlineTextField'
+import Editor from './Editor'
 const validInputList = {
   key: {
     label: 'Slug',
@@ -34,10 +35,10 @@ const validInputList = {
     type: 'date',
     required: true,
   },
-  body: {
-    label: 'Apresentação',
-    type: 'text'
-  },
+  // body: {
+  //   label: 'Apresentação',
+  //   type: 'text'
+  // },
   volume: {
     label: 'Volume',
     type: 'int'
@@ -65,22 +66,22 @@ const validInputList = {
 }
 
 function isValidDate(dateString) {
-  var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+  var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/
   if(!regex_date.test(dateString)) {
-    return false;
+    return false
   }
-  var parts   = dateString.split("-");
-  var day     = parseInt(parts[2], 10);
-  var month   = parseInt(parts[1], 10);
-  var year    = parseInt(parts[0], 10);
+  var parts   = dateString.split("-")
+  var day     = parseInt(parts[2], 10)
+  var month   = parseInt(parts[1], 10)
+  var year    = parseInt(parts[0], 10)
   if(year < 1000 || year > 3000 || month == 0 || month > 12) {
-    return false;
+    return false
   }
-  var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+  var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
   if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
-    monthLength[1] = 29;
+    monthLength[1] = 29
   }
-  return day > 0 && day <= monthLength[month - 1];
+  return day > 0 && day <= monthLength[month - 1]
 }
 
 
@@ -99,12 +100,12 @@ const validate = values => {
 }
 
 const cleanObject = (obj) => {
-  let newObj = {};
+  let newObj = {}
   Object.keys(obj).forEach((prop) => {
     if (Array.isArray(obj[prop]) && obj[prop]) 
-    if (obj[prop]) { newObj[prop] = obj[prop]; }
-  });
-  return newObj;
+    if (obj[prop]) { newObj[prop] = obj[prop] }
+  })
+  return newObj
 }
 
 const styles = theme => ({
@@ -125,6 +126,7 @@ const styles = theme => ({
 class IssueForm extends Component {
   state = {
     uploaded: null,
+    editor: '',
   }
 
   clearUpload = () => this.setState({ uploaded: null })
@@ -135,19 +137,29 @@ class IssueForm extends Component {
     this.setState({ uploaded })
   }
 
+  onEditorStateChange = (editor, change, blur) => {
+    console.log('EDITOR', editor)
+    blur('body')
+    change('body', editor)
+    this.setState({ editor })
+  }
+
   render() {
+    const { editorState } = this.state
     const { classes, onSubmit, issue, publishCall, publish } = this.props
     let formatedIssue = {}
     if (issue) {
+      /* Organize by issue */
       Object.keys(issue).map(i => {
         if (isISODate(issue[i])) {
+          /* Fix date */
           formatedIssue[i] = issue[i].split('T')[0]
         } else {
           formatedIssue[i] = issue[i]
         }
       }) 
     }
-    console.log('ISSUE', issue)
+    // console.log('ISSUE', issue)
     return (
       <Form
         initialValues={issue ? formatedIssue : {}}
@@ -157,8 +169,10 @@ class IssueForm extends Component {
             Object.keys(e).map(i => {
               if (i === valid && e[i] !== null) {
                 if (isValidDate(e[i])) {
+                  /* parse date */
                   cleanList[i] = new Date(e[i]).toISOString()
                 } else if (0 === e[i] % (!isNaN(parseFloat(e[i])) && 0 <= ~~e[i])) {
+                  /* parse ints */
                   cleanList[i] = parseInt(e[i])
                 } else {
                   cleanList[i] = e[i]
@@ -166,7 +180,6 @@ class IssueForm extends Component {
               }
             })
           })
-          
           console.log('cleanList', cleanList)
           await onSubmit(cleanList)
           this.clearUpload()
@@ -180,7 +193,7 @@ class IssueForm extends Component {
                 <Typography component="h4" variant="h4">Capa</Typography>
               </div>
               <div className={classes.column}>
-                {issue ? (issue.logo ? <img src={issue.logo} /> : <h4>Sem logo...</h4>) : null}
+                {issue ? (issue.logo ? <img src={issue.logo} /> : <h4>Sem imagem de capa...</h4>) : null}
                 {!issue ? (this.state.uploaded ? <img src={this.state.uploaded} /> : <h4>Sem logo...</h4>) : null}
               </div>
               <div className={classNames(classes.column, classes.helper)}>
@@ -214,6 +227,21 @@ class IssueForm extends Component {
                   </div>
                 ))
               }
+              <Field name="image">
+                {(fieldprops) => <Editor
+                  {...fieldprops}
+                  editorState={issue.body}
+                  onEditorStateChange={e => this.onEditorStateChange(e, change, blur)}
+                /> }
+              </Field>
+              <Divider />
+              {/* <Editor
+                editorState={editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={this.onEditorStateChange}
+              /> */}
               <Divider />
               <Button size="small">Cancel</Button>
               <Button size="small" color="primary" type="submit" disabled={pristine || invalid}>
